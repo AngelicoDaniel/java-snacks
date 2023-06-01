e7f5ff50fee0786dc4a8cbd177fe645c80a1aba3
Daniel <angelicodaniel93@gmail.com>
2023-06-01 14:03:40 +0200

    first commit

---
+++ src/org/lessons/java/Snack1.java
@@ -0,0 +1,8 @@
+package org.lessons.java;
+
+public class Snack1 {
+    public static void main(String[] args) {
+        //Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
+
+    }
+}
